9eb6123fcd9436150a30dd1137d6ca0adc446602
jadTahaGit <jadtaha.de@gmail.com>
2023-07-05 20:14:17 +0200

    08-truthyAndFalsyValues

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-const YearStr = '2000';
+// "", 0 , undefined, null, NaN
 
-console.log(Number(YearStr), YearStr);
-console.log(Number(YearStr) + 23);
-console.log(Number('Jad'));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean('Jad'));
+console.log(Boolean({}));
+console.log(Boolean(0));
+console.log(Boolean(100));
 
-console.log(String(24) + 16); // "24" + "16"
-
-console.log('I am ' + 23 + ' years Old');
-console.log('I am ' + String(23) + ' years Old');
-
-console.log('23' - '10' - 3);
-console.log('23' + '10' + 3);
-console.log('23' * '10');
-console.log('23' / '10');
-
-// Guess The Number:
-
-let n = '2' + 2; //22
-n = n - 1; //21
-console.log(n);
-
-// console.log(2 + 3 + 3 + '5');
+let width = 100;
+if (width) {
+  console.log(`YaaaHooo width is ${width}`);
+} else {
+  console.log(`Ooops width is ${Boolean(width)}`);
+}
