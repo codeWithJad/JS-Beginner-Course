a45715c993d428ec2617cc2e990fa52fe569272a
jadTahaGit <jadtaha.de@gmail.com>
2023-07-05 14:42:25 +0200

    07-Conversion & Coercion

---
+++ script.js
@@ -0,0 +1,23 @@
+const YearStr = '2000';
+
+console.log(Number(YearStr), YearStr);
+console.log(Number(YearStr) + 23);
+console.log(Number('Jad'));
+
+console.log(String(24) + 16); // "24" + "16"
+
+console.log('I am ' + 23 + ' years Old');
+console.log('I am ' + String(23) + ' years Old');
+
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '10');
+console.log('23' / '10');
+
+// Guess The Number:
+
+let n = '2' + 2; //22
+n = n - 1; //21
+console.log(n);
+
+// console.log(2 + 3 + 3 + '5');
